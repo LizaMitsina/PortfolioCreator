1fa547f1cd5fb449ff27ea79e5cb19de1724a1a9
LizaMitsina <elizabethmitsina@gmail.com>
2022-09-15 18:21:03 +0300

    add pop-up

--- js.js
+++ js.js
@@ -1,20 +1,32 @@
 var link = document.querySelector(".open-call");
 var popup = document.querySelector(".pop-up-bg");
 var close = document.querySelector(".cross");
+var closebtn = document.querySelector(".cross-btn");
+var linklets = document.querySelector(".lets");
+var black = document.getElementById("black");
 
 console.log(link);
+console.log(linklets);
+console.log(black);
 console.log(popup);
 
 link.addEventListener("click", (evt) => {
   evt.preventDefault();
   popup.style.display = "block";
 });
-
+linklets.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    popup.style.display = "block";
+  });
+black.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    popup.style.display = "block";
+  });
 close.addEventListener("click", (evt) => {
   evt.preventDefault();
   popup.style.display = "none";
 });
-close.addEventListener("click", (evt) => {
+closebtn.addEventListener("click", (evt) => {
     evt.preventDefault();
     popup.style.display = "none";
   });
