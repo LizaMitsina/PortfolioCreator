babfbf6dfad718f5d92ecd3f4b9e9baf3ef90bb1
lizamitsina <lizzimitsina@gmail.com>
2022-09-15 16:08:01 +0300

    add js

---
+++ js.js
@@ -0,0 +1,16 @@
+var link = document.querySelector(".open-call");
+var popup = document.querySelector(".pop-up-bg");
+var close = document.querySelector(".cross");
+
+console.log(link);
+console.log(popup);
+
+link.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  popup.style.display = "block";
+});
+
+close.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  popup.style.display = "none";
+});
