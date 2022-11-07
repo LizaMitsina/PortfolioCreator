0c331b9f7f1648fc669022edc556703956b57d6d
LizaMitsina <elizabethmitsina@gmail.com>
2022-11-07 13:52:31 +0300

    add js slider

--- js.js
+++ js.js
@@ -15,18 +15,42 @@ link.addEventListener("click", (evt) => {
   popup.style.display = "block";
 });
 linklets.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    popup.style.display = "block";
-  });
+  evt.preventDefault();
+  popup.style.display = "block";
+});
 black.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    popup.style.display = "block";
-  });
+  evt.preventDefault();
+  popup.style.display = "block";
+});
 close.addEventListener("click", (evt) => {
   evt.preventDefault();
   popup.style.display = "none";
 });
 closebtn.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    popup.style.display = "none";
-  });
+  evt.preventDefault();
+  popup.style.display = "none";
+});
+
+var next = document.querySelector(".next");
+var second_slider = document.querySelector(".second-slider");
+var first_slider = document.querySelector(".first-slider");
+var prev = document.querySelector(".prev");
+
+console.log(next);
+console.log(second_slider);
+console.log(first_slider);
+
+next.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  second_slider.style.display = "block";
+  first_slider.style.display = "none";
+});
+
+prev.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  prev.classList.add("active");
+  first_slider.style.display = "block";
+  second_slider.style.display = "none";
+  prev.classList.add("active");
+});
+
